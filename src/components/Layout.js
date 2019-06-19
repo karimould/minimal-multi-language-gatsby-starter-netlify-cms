cb405e2f6a073f35c18cee8b7f2126f1e1c4c6c3
Karim Ould Mahieddine <karim_om@me.com>
2019-06-20 00:25:17 +0200

    small refactor

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,42 +1,33 @@
 import React from 'react'
 import Helmet from 'react-helmet'
-import { StaticQuery, graphql } from "gatsby"
+import PropTypes from 'prop-types'
 
-const TemplateWrapper = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query HeadingQuery {
-          site {
-            siteMetadata {
-              title,
-              description,
-            }
-          }
-        }
-    `}
-    render={data => (
-      <div>
-        <Helmet>
-          <html lang="en" />
-          <title>{data.site.siteMetadata.title}</title>
-          <meta name="description" content={data.site.siteMetadata.description} />
-          
-          <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png" />
-	        <link rel="icon" type="image/png" href="/img/favicon-32x32.png" sizes="32x32" />
-	        <link rel="icon" type="image/png" href="/img/favicon-16x16.png" sizes="16x16" />
-	
-	        <link rel="mask-icon" href="/img/safari-pinned-tab.svg" color="#ff4400" />
-	        <meta name="theme-color" content="#fff" />
+const Layout = ({ children, title, description, locale, type='website' }) => (
+  <>
+    <Helmet>
+      <html lang={locale} />
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" href="/img/favicon-32x32.png" sizes="32x32" />
+      <link rel="icon" type="image/png" href="/img/favicon-16x16.png" sizes="16x16" />
+      <link rel="mask-icon" href="/img/safari-pinned-tab.svg" color="#ff4400" />
+      <meta name="theme-color" content="#fff" />
+      <meta property="og:type" content={type} />
+      <meta property="og:title" content={title} />
+      <meta property="og:url" content="/" />
+      <meta property="og:image" content="/img/og-image.jpg" />
+    </Helmet>
+    <div>{children}</div>
+  </>
+  )
 
-	        <meta property="og:type" content="business.business" />
-          <meta property="og:title" content={data.site.siteMetadata.title} />
-          <meta property="og:url" content="/" />
-          <meta property="og:image" content="/img/og-image.jpg" />
-        </Helmet>
-        <div>{children}</div>
-      </div>
-    )}
-  />
-)
+  Layout.propTypes = {
+    children: PropTypes.node.isRequired,
+    title: PropTypes.string,
+    description: PropTypes.string,
+    locale: PropTypes.string,
+    type: PropTypes.string,
+  }
 
-export default TemplateWrapper
+export default Layout
